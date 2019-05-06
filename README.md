b125398f31bbc3fac702dcd9c3096735f781d7d2
Erlend <erlend@landroe.com>
2019-05-06 17:26:27 +0200

    Inject the stores.xml path

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # StoreLocator
+
+## Configure stores input
+
+To properly load the store input file, set its path in appsettings.json on the node `AppSettings::StoreFilePath`.
---
+++ src/StoreLocator/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreLocator
+{
+    public class AppSettings
+    {
+        public string StoreFilePath { get; set; }
+    }
+}
